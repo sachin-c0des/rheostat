d2a782693efc0c697cab1ae83caa34daf16428e1
Arjun Rathi <85490710+arjun-rathi@users.noreply.github.com>
2021-12-13 17:35:02 +0530

    Deleted 1 test from linear-test.js

--- test/algorithms/linear-test.js
+++ test/algorithms/linear-test.js
@@ -3,14 +3,7 @@ import casual from 'casual';
 import geometric from '../../src/algorithms/geometric';
 
 describe('geometric algorithm', () => {
-  it('New Inverse Function check', () => {
-    const min = casual.integer(0, 100);
-    const max = casual.integer(900, 1000);
-    const originalPosition = casual.integer(100, 900);
-    const value = geometric.getValue(originalPosition, min, max);
-    const positionFromValue = geometric.getPosition(value, min, max);
-    assert.equal(Math.round(positionFromValue), originalPosition);
-  });
+  
   it('Version - 2 : of Inverse Function check', () => {
     const min = 10;
     const max = 1000;
