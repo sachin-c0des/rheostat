58c5013b0ae5270644b611eba6561fb1c7032407
Arjun Rathi <85490710+arjun-rathi@users.noreply.github.com>
2022-02-01 20:57:33 +0530

    Failing a test

--- test/algorithms/linear-test.js
+++ test/algorithms/linear-test.js
@@ -6,7 +6,7 @@ describe('linear algorithm', () => {
   
   it('Version - 2 : of Inverse Function check', () => {
     const min = 10;
-    const max = 1000;
+    const max = 11;
     const value = 358;
     const positionFromValue = geometric.getPosition(value, min, max);
     assert.equal(value, geometric.getValue(positionFromValue, min, max));
