25ee4551fd02d9c0b1948066443a42e2d11b9a03
Arjun Rathi <85490710+arjun-rathi@users.noreply.github.com>
2022-02-02 23:26:10 +0530

    Commit 6 - Geometric test

--- test/algorithms/geometric-test.js
+++ test/algorithms/geometric-test.js
@@ -1,7 +1,6 @@
 import { assert } from 'chai';
 import casual from 'casual';
 import geometric from '../../src/algorithms/geometric';
-
 describe('geometric algorithm', () => {
   it('should have inverse functions for getPosition and getValue', () => {
     const min = casual.integer(0, 100);
@@ -11,7 +10,6 @@ describe('geometric algorithm', () => {
     const positionFromValue = geometric.getPosition(value, min, max);
     assert.equal(Math.round(positionFromValue), originalPosition);
   });
-
   it('should have inverse functions for getValue and getPosition', () => {
     const min = 10;
     const max = 1000;
@@ -19,7 +17,6 @@ describe('geometric algorithm', () => {
     const positionFromValue = geometric.getPosition(value, min, max);
     assert.equal(value, geometric.getValue(positionFromValue, min, max));
   });
-
   it('should handle the minimum end of the range correctly', () => {
     const min = casual.integer(0, 99);
     const max = casual.integer(100, 1000);
@@ -28,7 +25,6 @@ describe('geometric algorithm', () => {
     const positionFromValue = geometric.getPosition(value, min, max);
     assert.equal(Math.round(positionFromValue), 0);
   });
-
   it('should handle the maximum end of the range correctly', () => {
     const min = casual.integer(0, 899);
     const max = casual.integer(900, 1000);
@@ -37,7 +33,6 @@ describe('geometric algorithm', () => {
     const positionFromValue = geometric.getPosition(value, min, max);
     assert.equal(Math.round(positionFromValue), 100);
   });
-
   it('should provide correct values for nice integers in the middle of a range', () => {
     const min = 0;
     const max = 1024;
